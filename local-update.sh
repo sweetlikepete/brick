986fbfcdd828241d82b17829d52e9021345faa51
Peter Bodechtel <sweetlikepete@gmail.com>
2019-02-15 13:23:35 +0200

    version bump

--- local-update.sh
+++ local-update.sh
@@ -1,8 +1,8 @@
 # local install eslint-config
 cd ~/code/brick-test/brick/packages/eslint-config
 npm pack
-mv sweetlikepete-eslint-config-1.0.13.tgz ~/code/brick-test
+mv sweetlikepete-eslint-config-1.0.14.tgz ~/code/brick-test
 cd ~/code/brick-test/brick
-yarn add ~/code/brick-test/sweetlikepete-eslint-config-1.0.13.tgz
+yarn add ~/code/brick-test/sweetlikepete-eslint-config-1.0.14.tgz
 # eslint-config peer dependencies
 yarn add @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-plugin-array-func eslint-plugin-compat eslint-plugin-eslint-comments eslint-plugin-import eslint-plugin-no-loops eslint-plugin-node eslint-plugin-optimize-regex eslint-plugin-promise eslint-plugin-react eslint-plugin-security eslint-plugin-unicorn typescript
